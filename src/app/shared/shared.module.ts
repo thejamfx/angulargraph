4fc112c43989cd018b1f039a6b7e102c5f1c35fa
Simon Ly <simon@r.recruit.co.jp>
2018-07-03 16:07:40 +0900

    Add graph.js and graph component scaffolding

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { GraphComponent } from './components/graph/graph.component';
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [GraphComponent]
+})
+export class SharedModule { }
